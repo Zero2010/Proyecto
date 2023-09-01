70c5f59e34a93360942c3a2ed1dc8e225c96b9cb
Zero2010 <mfelipe.isla.f@gmail.com>
2023-09-01 14:07:29 -0400

    Se agrefo proyectos .core

---
+++ .Core/MachineLearningFeel/MachineLearningFeel/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MachineLearningFeel;
+
+
+Console.WriteLine("Hello, World!");
+
+//Load sample data
+var sampleData = new MLModel1.ModelInput()
+{
+    Rev_id = 2.429755E+08F,
+    Comment = @"You are just great",
+    Year = 2005F,
+    Logged_in = false,
+    Ns = @"article",
+    Sample = @"random",
+    Split = @"train",
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+
+Console.WriteLine(result.PredictedLabel);
